5f2bc9f118fd6483e0ffd24b76d5724806716d1b
blaziinq <blaziinq@hotmail.com>
2022-06-12 22:17:03 +0100

    banner changes
    made banner only appear when on homepage, otherwise, only the navbar is displayed

--- header-and-footer/changer-header_footer.js
+++ header-and-footer/changer-header_footer.js
@@ -1,28 +1,38 @@
 welcome_text = "Welcome to my website!";
 
 function header_large() {
-  var header_section_text =
-    `
+  if (window.location.pathname == "/index.html") {
+    var header_section_text =
+      `
     <div style="overflow: hidden">
     <h1 id="first-title">` +
-    welcome_text +
-    `</h1>
+      welcome_text +
+      `</h1>
     </div>
     <div id="navbar">
       <a href="index.html" class="headerLink" style="float: left">Home</a>
       <a href="about.html" class="headerLink">About</a>
       <a href="projects.html" class="headerLink">Projects</a>
     </div>`;
+  } else {
+    var header_section_text = `
+    <div id="navbar">
+      <a href="index.html" class="headerLink" style="float: left">Home</a>
+      <a href="about.html" class="headerLink">About</a>
+      <a href="projects.html" class="headerLink">Projects</a>
+    </div>`;
+  }
 
   document.getElementById("header").innerHTML = header_section_text;
 }
 function header_small() {
-  var header_section_text =
-    `
+  if (window.location.pathname == "/index.html") {
+    var header_section_text =
+      `
     <div style="overflow: hidden">
     <h1 id="first-title">` +
-    welcome_text +
-    `</h1>
+      welcome_text +
+      `</h1>
     </div>
     <div id="navbar">
       <a href="index.html" class="headerLink" style="float: left">Home</a>
@@ -30,6 +40,15 @@ function header_small() {
       <a href="about.html" class="headerLink">About</a>
     </div>
   `;
+  } else {
+    var header_section_text = `
+      <div id="navbar">
+        <a href="index.html" class="headerLink" style="float: left">Home</a>
+        <a href="projects.html" class="headerLink">Projects</a>
+        <a href="about.html" class="headerLink">About</a>
+      </div>
+`;
+  }
 
   document.getElementById("header").innerHTML = header_section_text;
 }
@@ -82,4 +101,7 @@ window.addEventListener("resize", function () {
 window.onload = function Start() {
   resize_check();
   footer();
+  if (window.location.pathname == "" || window.location.pathname == "/") {
+    document.location.href = "index.html";
+  }
 };
