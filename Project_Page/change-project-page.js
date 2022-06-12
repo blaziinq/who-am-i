15930bbb9d3bf11c86d7dc70d208448ea741b13b
blaziinq <blaziinq@hotmail.com>
2022-06-12 16:15:48 +0100

    Basis for project page
    Added a bunch of temporary projects to fill the space, along with horizontal scrolling

--- Project_Page/change-project-page.js
+++ Project_Page/change-project-page.js
@@ -1,5 +1,18 @@
-function Update() {}
+const desired_scroll_amount = window.innerWidth / 2 + 650;
 
-window.setInterval(function () {
-  Update();
-}, 1);
+function Start() {
+  document.getElementById("scrollbar").scroll(desired_scroll_amount, 0);
+}
+
+// cross browser way to add an event listener
+function addListener(event, obj, fn) {
+  if (obj.addEventListener) {
+    obj.addEventListener(event, fn, false); // modern browsers
+  } else {
+    obj.attachEvent("on" + event, fn); // older versions of IE
+  }
+}
+
+addListener("load", window, function () {
+  Start(); // calls the start function when the page is loaded
+});
